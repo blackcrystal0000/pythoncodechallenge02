f95bd8452509c19605bd8de907b2d9aa275d6c8a
Crystal <muthonigachanja2000@gmail.com>
2023-09-05 02:09:16 -0700

    Creator engine

--- seed.py
+++ seed.py
@@ -3,3 +3,10 @@ from sqlalchemy import create_engine
 from faker import Faker
 from Restaurant import Restaurant, Review, Customer
 import random
+
+- Create an engine and session for the database.
+
+
+creator_engine = create_engine('sqlite:///restaurant_reviews.db')
+Session = sessionmaker(bind=creator_engine)
+session = Session()
