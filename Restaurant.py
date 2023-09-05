0c2846c5975b4e19eab2e5f6e5db1b4be67343c2
Crystal <muthonigachanja2000@gmail.com>
2023-09-05 00:22:23 -0700

    Comments

--- Restaurant.py
+++ Restaurant.py
@@ -1,3 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create a base class for our models to inherit from.
 Base = declarative_base()
+
+# Define the `Restaurant` class, which represents a restaurant in our domain.
