03a9b0fde0f918a4cdabe325928be2ca7703a151
Crystal <muthonigachanja2000@gmail.com>
2023-09-05 00:40:43 -0700

    Restaurant tables

--- Restaurant.py
+++ Restaurant.py
@@ -4,3 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 # Define the `Restaurant` class, which represents a restaurant in our domain.
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+
+    reviews = relationship("Review", back_populates="restaurant")
+    customers = relationship("Customer", secondary="reviews")
